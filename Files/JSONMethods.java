b21884bc52cb500e9a958a9b344dd5e9ede964a7
GetThisBread <48595349+GetThisBread@users.noreply.github.com>
2019-03-15 10:30:18 -0400

    Add files via upload

---
+++ Files/JSONMethods.java
@@ -0,0 +1,36 @@
+package store;
+//Ben Raphael
+public class JSONMethods {
+
+	public String title;
+	public double cost;
+	public String img_url;
+	public String desc;
+	
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public double getCost() {
+		return cost;
+	}
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+	public String getImg_url() {
+		return img_url;
+	}
+	public void setImg_url(String img_url) {
+		this.img_url = img_url;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	
+}
